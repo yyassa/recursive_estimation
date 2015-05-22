b5c9366d4bf42ed33c0127850eb18ba99761369b
surberj <surberj@student.ethz.ch>
2015-05-22 20:02:12 +0200

    no important changes

--- PF/test_pf.m
+++ PF/test_pf.m
@@ -2,12 +2,12 @@
 
 
 % number of runs:
-n_runs = 10;
+n_runs = 20;
 result = zeros(1,n_runs);
 time   = zeros(1,n_runs);
 
 for i=1:n_runs
-[result(i),time(i)] = run(0,0)
+[result(i),time(i)] = run(0,0);
 countdown = n_runs - i
 end
 
